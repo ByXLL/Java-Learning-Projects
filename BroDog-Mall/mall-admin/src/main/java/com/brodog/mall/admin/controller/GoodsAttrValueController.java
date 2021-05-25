ef38d62164567e97a7c1bf47bfb320640aba6896
BY-LIN\By-Lin <by_xll@163.com>
2021-05-25 09:52:16 +0800

    完善商品属性值接口控制器

--- BroDog-Mall/mall-admin/src/main/java/com/brodog/mall/admin/controller/GoodsAttrValueController.java
+++ BroDog-Mall/mall-admin/src/main/java/com/brodog/mall/admin/controller/GoodsAttrValueController.java
@@ -1,9 +1,13 @@
 package com.brodog.mall.admin.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.brodog.mall.admin.dto.goods.GoodsAttrValueAddDto;
+import com.brodog.mall.admin.service.impl.GoodsAttrValueServiceImpl;
+import com.brodog.mall.common.entity.ApiResult;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.RestController;
+import javax.validation.Valid;
+import javax.websocket.server.PathParam;
 
 /**
  * <p>
@@ -14,8 +18,42 @@ import org.springframework.web.bind.annotation.RestController;
  * @since 2021-05-24
  */
 @RestController
-@RequestMapping("/goods-attr-value")
+@RequestMapping("/goodsAttrValue")
 public class GoodsAttrValueController {
+    private final GoodsAttrValueServiceImpl service;
 
+    public GoodsAttrValueController(GoodsAttrValueServiceImpl service) {
+        this.service = service;
+    }
+
+    @PostMapping("/add")
+    public ApiResult add(@Valid @RequestBody GoodsAttrValueAddDto goodsAttrValueAddDto) {
+        return service.insert(goodsAttrValueAddDto);
+    }
+
+    @PostMapping("/delById")
+    public ApiResult deleteById(@PathParam("id") Long id) {
+        return service.deleteById(id);
+    }
+
+    @PostMapping("/delByAttrId")
+    public ApiResult delete(@PathParam("attrId") Long attrId) {
+        return service.deleteByAttrId(attrId);
+    }
+
+    @PostMapping("/deleteByGoodsId")
+    public ApiResult deleteByGoodsId(@PathParam("goodsId") Long goodsId) {
+        return service.deleteByGoodsId(goodsId);
+    }
+
+    @GetMapping("/getById")
+    public ApiResult selectById(@PathParam("id") Long id) {
+        return service.selectById(id);
+    }
+
+    @GetMapping("/getByGoodsId")
+    public ApiResult selectByGoodsId(@PathParam("goodsId") Long goodsId) {
+        return service.selectByGoodsId(goodsId);
+    }
 }
 
